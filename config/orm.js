cf39e39f76b011f641caebc7aeb5d021e33bd934
syin <shaomingyinut@gmail.com>
2018-09-17 07:21:11 -0400

    added updateOne fn in orm

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 // import MySQL connection
 var connection = require('./connection.js');
 
-// helper function
+// helper function to create a series of comma separated question marks. This acts as SQL placeholders
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -9,6 +9,29 @@ function printQuestionMarks(num) {
     arr.push('?');
   }
 
+  return arr.toString();
+}
+
+// helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + '=' + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
@@ -42,7 +65,23 @@ var orm = {
     );
   },
 
-  updateOne: function() {}
+  updateOne: function(table, objColVals, condition, cb) {
+    // objColVals example - {name: panther, sleepy: true}
+    connection.query(
+      'update ' +
+        table +
+        ' set ' +
+        objToSql(objColVals) +
+        ' where ' +
+        condition,
+      function(err, data) {
+        if (err) {
+          throw err;
+        }
+        cb(data);
+      }
+    );
+  }
 };
 
 module.exports = orm;
