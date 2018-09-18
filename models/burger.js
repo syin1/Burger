62413645b746971bdd7325781e23b5dd2381e106
syin <shaomingyinut@gmail.com>
2018-09-18 05:30:56 -0400

    added update fn in model file

--- models/burger.js
+++ models/burger.js
@@ -12,6 +12,12 @@ var burger = {
     orm.insertOne('burgers', cols, vals, function(res) {
       cb(res);
     });
+  },
+
+  update: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
   }
 };
 
