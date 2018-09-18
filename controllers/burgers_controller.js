c6f5d2a59580558574a1076d9d3311364ca1c4c5
syin <shaomingyinut@gmail.com>
2018-09-18 06:17:27 -0400

    added delete route in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -37,5 +37,18 @@ router.put('/api/burgers/:id', function(req, res) {
   });
 });
 
+router.delete('/api/burgers/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows === 0) {
+      // no rows changed, ID does not exist
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
 // export routes for server.js
 module.exports = router;
