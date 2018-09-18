7dd2cbf0889126ffa4d9541d6e3316f6723e91d9
syin <shaomingyinut@gmail.com>
2018-09-18 05:29:02 -0400

    added create fn in model file

--- models/burger.js
+++ models/burger.js
@@ -6,6 +6,12 @@ var burger = {
     orm.selectAll('burgers', function(res) {
       cb(res);
     });
+  },
+
+  create: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
+    });
   }
 };
 
