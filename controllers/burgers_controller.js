fa9a8d3282d2306376e36238acb5b0e976e09771
syin <shaomingyinut@gmail.com>
2018-09-18 06:00:27 -0400

    added post route in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,5 +14,15 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/api/burgers', function(req, res) {
+  burger.create(
+    ['burger_name', 'devoured'],
+    [req.body.burger_name, req.body.devoured],
+    function(result) {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
 // export routes for server.js
 module.exports = router;
