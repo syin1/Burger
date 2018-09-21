07d8ff43ce01b28baf726e7bafb3b8743b5d33f9
syin <shaomingyinut@gmail.com>
2018-09-20 22:28:07 -0400

    reloads page after devouring burger or adding new burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,7 +19,8 @@ router.post('/api/burgers', function(req, res) {
     ['burger_name', 'devoured'],
     [req.body.burger_name, req.body.devoured],
     function(result) {
-      res.json({ id: result.insertId });
+      // res.json({ id: result.insertId });
+      res.redirect('/');
     }
   );
 });
@@ -32,23 +33,25 @@ router.put('/api/burgers/:id', function(req, res) {
       // no rows changed, ID does not exist
       return res.status(404).end();
     } else {
-      res.status(200).end();
+      // res.status(200).end();
+      res.redirect('/');
     }
   });
 });
 
-router.delete('/api/burgers/:id', function(req, res) {
-  var condition = 'id = ' + req.params.id;
+// router.delete('/api/burgers/:id', function(req, res) {
+//   var condition = 'id = ' + req.params.id;
 
-  burger.delete(condition, function(result) {
-    if (result.affectedRows === 0) {
-      // no rows changed, ID does not exist
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows === 0) {
+//       // no rows changed, ID does not exist
+//       return res.status(404).end();
+//     } else {
+//       // res.status(200).end();
+//       res.redirect('/');
+//     }
+//   });
+// });
 
 // export routes for server.js
 module.exports = router;
